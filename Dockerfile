90283c25dbac7a7bdf865bb41e27cf1883869868
Lionel Félicité <deogracia@free.fr>
2020-11-15 21:06:42 +0100

    add ghostscript & imagemagick

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine:3.11.6
 
 RUN apk update && \
-    apk add --no-cache texlive-full
+    apk add --no-cache texlive-full imagemagick ghostscript
 
 WORKDIR /latex
--- test.sh
+++ test.sh
@@ -5,7 +5,11 @@ apk update; apk add curl
 curl -fsSL https://goss.rocks/install | sh
 
 mkdir -p /latex/output && \
+  echo "Generate pdf with xelatex..." && \
   cd /latex/output && \
   xelatex /latex/doc.tex && \
+  echo "Generate pdf with xelatex... OK!" && \
   cd /latex && \
-  goss validate
+  echo "Run goss..." && \
+  goss validate --format documentation && \
+  echo "Run goss... OK!"
